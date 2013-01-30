e73e4ecaedd9320977289114fb179073281b4e40
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2013-01-30 19:45:44 -0200

    Added xmonad configuration file

---
+++ xmonad.hs
@@ -0,0 +1,18 @@
+import XMonad
+import XMonad.Actions.Volume
+import XMonad.Util.Dzen
+import Data.Map (fromList)
+import Data.Monoid (mappend)
+import Graphics.X11.ExtraTypes.XF86
+
+alert = dzenConfig return . show
+
+main = xmonad defaultConfig {
+    borderWidth = 2,
+    modMask = mod4Mask,
+    terminal = "gnome-terminal --hide-menubar",
+    keys = keys defaultConfig `mappend` \c -> fromList [
+        ((0, xF86XK_AudioLowerVolume), lowerVolume 4 >>= alert),
+        ((0, xF86XK_AudioRaiseVolume), raiseVolume 4 >>= alert)
+    ]
+}
