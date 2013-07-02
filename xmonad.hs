649a45c502d74d8cb06385570916fce67cf6ee88
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2013-07-02 14:42:05 -0300

    Removing volume key bindings (using gnome-session now)

--- xmonad.hs
+++ xmonad.hs
@@ -5,16 +5,10 @@ import Data.Map (fromList)
 import Data.Monoid (mappend)
 import Graphics.X11.ExtraTypes.XF86
 
-alert = dzenConfig return . show
-
 main = xmonad defaultConfig {
     borderWidth = 1,
     normalBorderColor = "#000000",
     focusedBorderColor = "#FF0000",
     modMask = mod4Mask,
-    terminal = "gnome-terminal --hide-menubar",
-    keys = keys defaultConfig `mappend` \c -> fromList [
-        ((0, xF86XK_AudioLowerVolume), lowerVolume 4 >>= alert),
-        ((0, xF86XK_AudioRaiseVolume), raiseVolume 4 >>= alert)
-    ]
+    terminal = "gnome-terminal --hide-menubar"
 }
