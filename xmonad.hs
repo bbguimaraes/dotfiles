9d2df1782c864933921a876bb865b5abe824073b
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2013-07-02 14:42:56 -0300

    Why not use haskell's strange style for dictionaries? Looks funny ü

--- xmonad.hs
+++ xmonad.hs
@@ -5,10 +5,10 @@ import Data.Map (fromList)
 import Data.Monoid (mappend)
 import Graphics.X11.ExtraTypes.XF86
 
-main = xmonad defaultConfig {
-    borderWidth = 1,
-    normalBorderColor = "#000000",
-    focusedBorderColor = "#FF0000",
-    modMask = mod4Mask,
-    terminal = "gnome-terminal --hide-menubar"
-}
+main = xmonad defaultConfig
+    { borderWidth = 1
+    , normalBorderColor = "#000000"
+    , focusedBorderColor = "#FF0000"
+    , modMask = mod4Mask
+    , terminal = "gnome-terminal --hide-menubar"
+    }
