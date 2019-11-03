f0e14fde1ca97842fdc077a237c44ecf013c37d7
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2019-11-03 20:33:38 +0000

    khal: update pass entry name

--- khal/vdirsyncer.sh
+++ khal/vdirsyncer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-p=$(pass show nextcloud/cal)
+p=$(pass show bbguimaraes.com/nextcloud/cal)
 while timeout 5m vdirsyncer --verbosity WARNING sync --force-delete <<< "$p"
 do sleep 5m; done
