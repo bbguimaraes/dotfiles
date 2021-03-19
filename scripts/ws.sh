75ed9bbbb6b7c7e7a3e66f027a4a71fc78268f7b
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2021-03-19 09:25:04 +0000

    scripts: ws

--- scripts/ws.sh
+++ scripts/ws.sh
@@ -5,9 +5,9 @@ if [[ "$#" -lt 1 ]]; then
     echo >&2 "Usage: $0 dir"
     exit 1
 fi
-name=$1
-dir=$name
-[[ -e "$dir" ]] || dir=$HOME/src/$name
+dir=$1
+[[ -e "$dir" ]] || dir=$HOME/src/$dir
+name=${1##*/}
 cd "$dir"
 tmux rename-window "$name"
 tmux split-window -c "$dir" 'git branch; git status; exec bash -i'
