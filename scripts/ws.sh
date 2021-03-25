d34380768c5808e935a040abb01e8e963c06d32b
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2021-03-25 11:40:08 +0000

    scripts/ws: support completion

--- scripts/ws.sh
+++ scripts/ws.sh
@@ -7,7 +7,17 @@ main() {
     [[ "$#" -gt 0 ]] || { echo >&2 "Usage: $0 dir"; return 1; }
     dir=$1
     case "$dir" in
+    complete) cmd_complete;;
     *) cmd_ws "$@";;
+    esac
+}
+
+cmd_complete() {
+    local line=($COMP_LINE)
+    local n=${#line[@]}
+    case "$n" in
+    1) compgen -W "$(ls "$DEFAULT_DIR")";;
+    2) compgen -W "$(ls "$DEFAULT_DIR")" "${line[$((n - 1))]}";;
     esac
 }
 
