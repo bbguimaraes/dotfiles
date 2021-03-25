cf4509948d4169654d8754bb1488b49e90411d5e
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2021-03-25 11:39:32 +0000

    scripts/ws: add command structure

--- scripts/ws.sh
+++ scripts/ws.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 set -euo pipefail
 
-if [[ "$#" -lt 1 ]]; then
-    echo >&2 "Usage: $0 dir"
-    exit 1
-fi
-dir=$1
-[[ -e "$dir" ]] || dir=$HOME/src/$dir
-name=${1##*/}
-cd "$dir"
-tmux rename-window "$name"
-tmux split-window -c "$dir" 'git branch; git status; exec bash -i'
-tmux select-layout main-vertical
-sleep 1
-exec vim
+DEFAULT_DIR=$HOME/src
+
+main() {
+    [[ "$#" -gt 0 ]] || { echo >&2 "Usage: $0 dir"; return 1; }
+    dir=$1
+    case "$dir" in
+    *) cmd_ws "$@";;
+    esac
+}
+
+cmd_ws() {
+    [[ -e "$dir" ]] || dir=$DEFAULT_DIR/$dir
+    name=${1##*/}
+    cd "$dir"
+    tmux rename-window "$name"
+    tmux split-window -c "$dir" 'git branch; git status; exec bash -i'
+    tmux select-layout main-vertical
+    sleep 1
+    exec vim
+}
+
+main "$@"
