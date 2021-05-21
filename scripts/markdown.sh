88b0c3b93a2c02a2239ffa93a3cc9f00de546084
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2021-05-21 19:15:55 +0000

    scripts/markdown: add modes

--- scripts/markdown.sh
+++ scripts/markdown.sh
@@ -1,4 +1,47 @@
 #!/bin/bash
 set -euo pipefail
 
-exec markdown -f fencedcode,footnote,links "$@"
+main() {
+    local cmd=
+    [[ "$#" -gt 0 ]] && { cmd=$1; shift; }
+    case "$cmd" in
+    simple) simple "$@";;
+    css) css "$@";;
+    hugo) hugo "$@";;
+    esac
+}
+
+simple() {
+    markdown -f fencedcode,footnote,links "$@"
+}
+
+css() {
+    cat <<'EOF'
+<style>
+    body {
+        max-width: 80ch;
+        margin-left: auto;
+        margin-right: auto;
+    }
+</style>
+EOF
+    simple "$@"
+}
+
+hugo() {
+    hugo_replace | css
+}
+
+hugo_replace() {
+    sed -e "$(cat <<'EOF'
+/^---$/,//d
+s,^{{< highlight .*>}},<code><pre>,
+s,^{{< /\s*highlight .*>}},</pre></code>,
+s,^{{< alert .*color="warning".*>}},<b>Warning: ,
+s,^{{< alert .*>}},<b>Note: ,
+s,^{{< /\s*alert .*>}},</b>,
+EOF
+)"
+}
+
+main "$@"
