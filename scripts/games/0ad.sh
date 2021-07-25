64f1f5d7688e1638129633e82bc1ee28e1969e01
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2021-07-25 07:55:33 +0000

    scripts: games

---
+++ scripts/games/0ad.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -euo pipefail
+
+main() {
+    local cmd=
+    [[ "$#" -gt 0 ]] && { cmd=$1; shift; }
+    case "$cmd" in
+    install) install "$@";;
+    build) build "$@";;
+    shell) common_shell "$@";;
+    *) echo >&2 "invalid command: $cmd"; return 1;;
+    esac
+}
+
+install() {
+    common_install "$1" \
+        base base-devel boost cmake enet fmt git gloox libpng libsodium \
+        libvorbis miniupnpc openal pulseaudio python rust sdl2 wxgtk3
+}
+
+build() {
+    common_exec "$1" \
+        --user "$COMMON_USER" \
+        --setenv SHELL=$SHELL \
+        --chdir /home/"$COMMON_USER" \
+        bash -c "$(cat <<'EOF'
+set -euo pipefail
+[[ -e 0ad ]] || git clone --depth 5 https://github.com/0ad/0ad.git
+cd 0ad
+pushd build/workspaces
+WX_CONFIG=wx-config-gtk3 ./update-workspaces.sh
+pushd gcc
+make
+EOF
+)"
+}
+
+dir=$(dirname "$BASH_SOURCE")
+source "$dir/common.sh"
+main "$@"
---
+++ scripts/games/openage.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -euo pipefail
+
+main() {
+    local cmd=
+    [[ "$#" -gt 0 ]] && { cmd=$1; shift; }
+    case "$cmd" in
+    install) install "$@";;
+    build) build "$@";;
+    shell) common_shell "$@";;
+    *) echo >&2 "invalid command: $cmd"; return 1;;
+    esac
+}
+
+install() {
+    common_install "$1" \
+        base base-devel cmake cython eigen git libepoxy opus opusfile \
+        pulseaudio python python-jinja python-lz4 python-numpy python-pillow \
+        python-pygments python-toml qt5-base qt5-declarative qt5-quickcontrols \
+        sdl2 sdl2_image ttf-dejavu
+}
+
+build() {
+    common_exec "$1" \
+        --user "$COMMON_USER" \
+        --setenv SHELL=$SHELL \
+        --chdir /home/"$COMMON_USER" \
+        bash -c "$(cat <<'EOF'
+set -euo pipefail
+[[ -e openage ]] || git clone https://github.com/SFTtech/openage.git
+cd openage
+git checkout v0.4.1
+./configure --mode=release --download-nyan
+make
+EOF
+)"
+}
+
+dir=$(dirname "$BASH_SOURCE")
+source "$dir/common.sh"
+main "$@"
