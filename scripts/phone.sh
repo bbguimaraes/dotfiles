96b8fa368f971ef1be4b0a3aed7db33f6fc11c9b
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2021-08-13 17:43:36 +0000

    scripts/phone: add `ls`/`pull` commands

--- scripts/phone.sh
+++ scripts/phone.sh
@@ -1,22 +1,51 @@
 #!/bin/bash
 set -euo pipefail
 
+ADDR=192.168.0.5
+PORT=2121
+ARGS=(--user anonymous:)
+ROOT=/storage/emulated/0
+
 main() {
     local cmd=
     [[ "$#" -gt 0 ]] && { cmd=${1:-}; shift; }
     case "$cmd" in
     push) push "$@";;
+    pull) pull "$@";;
+    ls) _ls "$@";;
     *) echo >&2 "invalid command: $cmd"; return 1;
     esac
 }
 
 push() {
-    local addr=192.168.0.5 port=2121 user=anonymous root=/storage/emulated/0
+    local x
     for x; do
         echo "$x"
-        curl "ftp://$addr:$port/$root/" \
-            --user "$user:" \
-            --upload-file "$x"
+        curl "${ARGS[@]}" --upload-file "$x" "ftp://$ADDR:$PORT/$ROOT/"
+    done
+}
+
+pull() {
+    local x
+    for x; do
+        x=$(basename "$x")
+        if [[ -e "$x" ]]; then
+            echo >&2 "file exists, not overwriting: $x"
+            return 1
+        fi
+    done
+    for x; do
+        echo "$x"
+        curl --ignore-content-length "${ARGS[@]}" "ftp://$ADDR:$PORT/$ROOT/$x" -o "$(basename "$x")"
+    done
+}
+
+_ls() {
+    [[ "$#" -eq 0 ]] && set -- ''
+    local x
+    for x; do
+        [[ "$x" ]] && echo "$x"
+        curl "${ARGS[@]}" "ftp://$ADDR:$PORT/$ROOT/$x"
     done
 }
 
