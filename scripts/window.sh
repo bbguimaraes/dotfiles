5db97165d2c109fc3ad131d9dd4c06da311c6788
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2021-08-23 09:10:04 +0000

    scripts: window

---
+++ scripts/window.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -euo pipefail
+
+main() {
+    local cmd=
+    [[ "$#" -gt 0 ]] && { cmd=$1; shift; }
+    case "$cmd" in
+    window) window "$@";;
+    vtr) vtr "$@";;
+    *) echo >&2 "invalid command: $cmd"; return 1;;
+    esac
+}
+
+window() {
+    local w ww wh sw sh cmd=
+    IFS=x read -r sw sh < <(xdpyinfo | awk '/^  dimensions:/{print$2}')
+    read -r w _ _ ww wh _ < <( \
+        xdotool selectwindow windowfocus getwindowgeometry --shell \
+            | sed 's/^.*=//' \
+            | paste -s)
+    for x; do
+        case "$x" in
+        tr) cmd=$(printf '%s\n%s' "$cmd" "windowmove $w $((sw - ww)) 0");;
+        tl) cmd=$(printf '%s\n%s' "$cmd" "windowmove $w 0 0");;
+        halve)
+            ww=$((ww / 2)); wh=$((wh / 2))
+            cmd=$(printf '%s\n%s' "$cmd" "windowsize $w $ww $wh") ;;
+        htr)
+            ww=$((ww / 2)); wh=$((wh / 2))
+            cmd=$(printf '%s\n%s' "$cmd" "windowsize $w $ww $wh")
+            cmd=$(printf '%s\n%s' "$cmd" "windowmove $w $((sw - ww)) 0");;
+        qtr)
+            ww=$((ww / 4)); wh=$((wh / 4))
+            cmd=$(printf '%s\n%s' "$cmd" "windowsize $w $ww $wh")
+            cmd=$(printf '%s\n%s' "$cmd" "windowmove $w $((sw - ww)) 0");;
+        quarter)
+            ww=$((ww / 4)); wh=$((wh / 4))
+            cmd=$(printf '%s\n%s' "$cmd" "windowsize $w $ww $wh") ;;
+        esac
+    done
+    xdotool - <<< "$cmd"
+}
+
+vtr() {
+    local w ww wh sw sh
+    IFS=x read -r sw sh < <(xdpyinfo | awk '/^  dimensions:/{print$2}')
+    w=$(xdotool getwindowfocus)
+    i3-msg --quiet 'floating enable; border none'
+    read -r ww wh < <(
+        xdotool getwindowgeometry --shell "$w" \
+            | awk -F = 'NR==4||NR==5{printf("%s ",$2)}END{print"\n"}')
+    while [[ $ww -gt $((sw / 3)) || $wh -gt $((sh / 3)) ]]; do
+        ww=$((ww / 2)); wh=$((wh / 2))
+    done
+    xdotool \
+        windowsize "$w" "$ww" "$wh" \
+        windowmove "$w" "$((sw - ww))" 0
+}
+
+main "$@"
