7fb94e1f0dc15d7fd63b2c4a686986ab1edb9b8c
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2021-11-08 11:56:57 +0000

    scripts: git

---
+++ scripts/git.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -euo pipefail
+
+main() {
+    local cmd
+    [[ "$#" -gt 0 ]] && { cmd=$1; shift; }
+    case "$cmd" in
+    authors) authors "$@";;
+    rebase) rebase "$@";;
+    *) echo >&2 "invalid command: $cmd"; return 1
+    esac
+}
+
+authors() {
+    git blame --porcelain "$@" \
+        | awk '$1 ~ /^author(-mail)?$/ { sub("^\\S+ ", ""); print; }' \
+        | awk 'NR % 2 { printf("%s ", $0); next } 1' \
+        | sort -u
+}
+
+rebase() {
+    local cmd
+    [[ "$#" -gt 0 ]] && { cmd=$1; shift; }
+    case "$cmd" in
+    backport) rebase_backport "$@";;
+    branches) rebase_branches "$@";;
+    *) echo >&2 "invalid command: rebase $cmd"; return 1
+    esac
+}
+
+rebase_branches() {
+    local x
+    git switch master
+    [[ "$#" -eq 0 ]] && set -- $(git branch --no-merged)
+    git rebase master "$1"
+    shift
+    for x; do
+        sleep 1 # for unique timestamps
+        git rebase master "$x"
+    done
+}
+
+rebase_backport() {
+    local rev=$1 cur base
+    rev=$(git rev-parse "$rev")
+    cur=$(git rev-parse HEAD)
+    base=$(git merge-base "$cur" "$rev")
+    if [[ "$base" != "$rev" ]]; then
+        echo >&2 'invalid ancestor'
+        return 1
+    fi
+    git stash push
+    git reset --hard "$rev"
+    git stash pop
+    git add -u
+    git commit --amend --no-edit
+    git cherry-pick "$rev..$cur"
+}
+
+main "$@"
