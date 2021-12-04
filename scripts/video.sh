d888ca8a4e7e5cfd102291d66adf0c6ea83b149f
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2021-12-04 13:59:41 +0000

    scripts/video: add audio conversion command

--- scripts/video.sh
+++ scripts/video.sh
@@ -8,6 +8,7 @@ main() {
     audio) youtube-dl \
         --extract-audio --audio-format vorbis \
         -o '%(title)s.%(ext)s' "$@";;
+    conv) conv "$@";;
     split) ~/n/comp/scripts/split_video.py "$@";;
     compress) compress "$@";;
     playlist) playlist "$@";;
@@ -20,6 +21,21 @@ compress() {
     exec ffmpeg \
         -threads "$(nproc)" -i "$in" \
         -acodec copy -quality realtime -speed 0 -crf 33 "$@"
+}
+
+conv() {
+    local cmd=
+    [[ "$#" -gt 0 ]] && { cmd=$1; shift; }
+    case "$cmd" in
+    audio) conv_audio "$@";;
+    *) echo >&2 "invalid command: conv $cmd"; return 1;;
+    esac
+}
+
+conv_audio() {
+    local in=$1
+    local out=${in%.*}.ogg
+    exec ffmpeg -i "$in" -vn "$out"
 }
 
 playlist() {
