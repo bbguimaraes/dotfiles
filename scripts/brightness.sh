3441f40f673ed98125169749fd456b173b85610d
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2022-04-10 12:35:16 +0000

    scripts/brightness: stop using obsolete script

--- scripts/brightness.sh
+++ scripts/brightness.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 set -euo pipefail
 
+CUR=/sys/class/backlight/intel_backlight/brightness
+MAX=/sys/class/backlight/intel_backlight/max_brightness
+
 main() {
-    local cmd=
-    [[ "$#" -gt 0 ]] && { cmd=$1; shift; }
+    [[ "$#" -eq 0 ]] && { brightness; return; }
+    local cmd=$1; shift
     case "$cmd" in
     gui) gui "$@";;
-    *) brightness "$@";;
+    *) brightness "$cmd" "$@";;
     esac
 }
 
 gui() {
-    local cur_f=/sys/class/backlight/intel_backlight/brightness
-    local max_f=/sys/class/backlight/intel_backlight/max_brightness
     local cur max
-    cur=$(<"$cur_f")
-    max=$(<"$max_f")
-    nngn_configure "i:0:$max:$cur:%1"$'\n' \
-        > /sys/class/backlight/intel_backlight/brightness
+    cur=$(<"$CUR")
+    max=$(<"$MAX")
+    nngn_configure "i:0:$max:$cur:brightness:%1"$'\n' > "$CUR"
 }
 
 brightness() {
     local d p
-    d=$(date +%H)
-    d=${d#0}
-    if [[ "$d" -lt 4 ]]; then p=25
-    elif [[ "$d" -lt 14 ]]; then p=50
-    elif [[ "$d" -lt 16 ]]; then p=25
-    else p=5; fi
-    exec sudo brightness "$p%"
+    if [[ "$#" -ne 0 ]]; then
+        if ! [[ "$1" =~ [0-9]+% ]]; then
+            echo >&2 "invalid percentage: $1"
+            return 1
+        fi
+        p=$1
+    else
+        d=$(date +%H)
+        d=${d#0}
+        if [[ "$d" -lt 4 ]]; then p=25
+        elif [[ "$d" -lt 14 ]]; then p=50
+        elif [[ "$d" -lt 16 ]]; then p=25
+        else p=5; fi
+    fi
+    exec awk -v "p=$p" '{printf "%d", $1 * p / 100.0}' < "$MAX" > "$CUR"
 }
 
 main "$@"
