f37f885a9d8b17631e1c899f15c2d6ed625e161a
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2022-04-23 21:30:30 +0000

    scripts: cd: fix path check

--- scripts/cd.sh
+++ scripts/cd.sh
@@ -5,7 +5,7 @@ DIR=${XDG_CONFIG_DIR:-$HOME/.config}/bbguimaraes/cd
 target=${1:-$HOME}
 target=$(readlink --canonicalize "$target")
 len=${#HOME}
-[[ "${target::$len}" != "$HOME" ]] && exit
+[[ "${target::$len + 1}" != "$HOME/" ]] && exit
 target=${target:$(($len+1))}
 file=$DIR/$target.sh
 [[ -e "$file" ]] && cat "$file"
