890a062ce995553f5be3347abd495d9c72c414d3
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2022-04-23 21:28:06 +0000

    scripts: arch: fix image build/push

--- scripts/arch.sh
+++ scripts/arch.sh
@@ -20,7 +20,7 @@ Commands:
 
     clean
     kernel revert
-    img
+    img [build|push]
 EOF
     return 1
 }
@@ -54,17 +54,21 @@ kernel_revert() {
 }
 
 img() {
+    local cmd=
+    [[ "$#" -gt 0 ]] && { cmd=$1; shift; }
+    case "$cmd" in
+    build) img_build "$@";;
+    push) img_push "$@";;
+    *) img build "$@"; img push "$@";;
+    esac
+}
+
+img_build() {
     local dir=${TMPDIR:-/tmp}/arch
     if [[ -e "$dir" ]]; then
         echo >&2 "$dir exists, not overwriting"
         return 1
     fi
-    img_build "$dir"
-    img_push "$dir"
-}
-
-img_build() {
-    local dir=$1
     sudo bash -s "$dir" <<'EOF'
 mkdir "$1/"
 pacstrap -cd "$1/" base
@@ -74,10 +78,13 @@ EOF
 }
 
 img_push() {
-    local dir=$1
-    ssh bbguimaraes.com -- sudo bash -ec < "$dir.tar.xz" '
-docker import - arch
-podman tag docker.io/library/arch arch'
+    local dir=${TMPDIR:-/tmp}/arch
+    local eval_args=$(printf %q 'eval "$@"')
+    local and=$(printf %q '&&')
+    pv "$dir.tar.xz" | ssh bbguimaraes.com -- sudo bash \
+        -c "$eval_args" bash \
+        podman import - arch "$and" \
+        podman tag docker.io/library/arch arch
 }
 
 main "$@"
