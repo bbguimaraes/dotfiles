09760ee76800bfba6525319da9119fce74f03847
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2022-04-24 13:20:37 +0000

    scripts: games: Mount & Blade - WF&S

---
+++ scripts/games/mab.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -euo pipefail
+
+main() {
+    local cmd=
+    [[ "$#" -gt 0 ]] && { cmd=$1; shift; }
+    case "$cmd" in
+    install) install "$@";;
+    build) build "$@";;
+    shell) common_shell "$@";;
+    run) common_shell "$@" \
+        wine '.wine/drive_c/GOG Games/Mount and Blade - With Fire and Sword/mb_wfas.exe';;
+    *) echo >&2 "invalid command: $cmd"; return 1;;
+    esac
+}
+
+install() {
+    common_install "$1" \
+        base lib32-libpulse lib32-mesa-libgl lib32-mpg123 wine winetricks
+}
+
+build() {
+    common_exec "$1" \
+        --user "$COMMON_USER" \
+        winetricks dlls d3dcompiler_47
+}
+
+dir=$(dirname "$BASH_SOURCE")
+source "$dir/common.sh"
+main "$@"
