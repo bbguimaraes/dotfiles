9b33611ee4a57e1197dc15fae547b9b9a41baf5a
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2022-05-11 10:33:47 +0000

    scripts: dev

---
+++ scripts/dev.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail
+
+main() {
+    [[ "$#" -eq 0 ]] && usage
+    local cmd=$1; shift
+    case "$cmd" in
+    *) usage;;
+    esac
+}
+
+usage() {
+    cat >&2 <<EOF
+Usage: $0 CMD
+
+Commands:
+EOF
+    return 1
+}
+
+main "$@"
