36533034a3ae5b3665dc4f9195d9a0e6ed5a3ecc
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2022-06-17 08:42:40 +0000

    scripts/sink: add host-based sink name mapping
    Also rewrite in Perl =)

---
+++ scripts/sink.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+sub main {
+    my $sink = shift;
+    my @sinks = `pactl list short sinks`;
+    return print @sinks if !$sink;
+    my $id = find_sink(map_sink_name($sink, `hostname`), \@sinks);
+    die "sink $sink not found" if !$id;
+    system "pactl set-default-sink $id";
+    foreach(`pactl list short sink-inputs`) {
+        my @f = split;
+        system "pactl move-sink-input $f[0] $id";
+    }
+}
+
+sub find_sink {
+    my ($sink, $sinks) = @_;
+    return $sink if $sink =~ m/^[0-9]+$/;
+    foreach(@$sinks) {
+        my @f = split;
+        return $f[0] if $f[1] =~ $sink;
+    }
+}
+
+sub map_sink_name {
+    my ($sink, $host) = @_;
+    if($host =~ /^rh/) {
+        if($sink eq "analog-stereo") {
+            return "sofhdadsp__sink";
+        } elsif($sink eq "hdmi-stereo") {
+            return "sofhdadsp_4__sink";
+        }
+    }
+}
+
+main @ARGV;
