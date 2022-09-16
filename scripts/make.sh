ce2e9759a35cd56566ddfc0cd2884f7834c3ab03
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2022-09-16 13:26:20 +0000

    make: add `make-pdf` alternative

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+bin=$0
+bin=$(basename "$0")
+bin=${bin%.*}
+
 test -v BUILD_DIR && set -- -C "$BUILD_DIR" "$@"
-exec make "$@"
+case "$(basename "$0")" in
+make-pdf)
+    make "$@"
+    pkill -HUP mupdf;;
+*)
+    exec make "$@";;
+esac
