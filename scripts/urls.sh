5f25f9611c1229a8deb84d3df2f9e62eb836c8e5
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2022-10-08 07:15:19 +0000

    scripts: urls

---
+++ scripts/urls.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -euo pipefail
+
+declare -A L=(
+    [posix]=https://pubs.opengroup.org/onlinepubs/9699919799/
+)
+
+main() {
+    [[ "$#" -eq 0 ]] && usage
+    local cmd=$1; shift
+    case "$cmd" in
+    menu) menu "$@";;
+    open) open "$@";;
+    *) usage;;
+    esac
+}
+
+usage() {
+    cat >&2 <<EOF
+Usage: $0 CMD [ARGS...]
+
+Commands:
+
+    open URL
+    menu
+EOF
+    return 1
+}
+
+menu() {
+    local OFS=$'\n' url
+    url=$(dmenu <<< ${!L[@]})
+    open "$url"
+}
+
+open() {
+    local url=${L[$1]}
+    if [[ "$url" ]]; then
+        xdg-open "$url"
+    else
+        echo >&2 "invalid url: $url"
+        return 1
+    fi
+}
+
+main "$@"
