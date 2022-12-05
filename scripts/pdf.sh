f6de882c32b94d023c82264eef60b6ad9c078ee0
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2022-12-05 08:51:46 +0000

    scripts/pdf: add command for two-per-sheet print

--- scripts/pdf.sh
+++ scripts/pdf.sh
@@ -6,6 +6,7 @@ main() {
     local cmd=$1; shift
     case "$cmd" in
     split) split "$@";;
+    two-per-sheet) two_per_sheet "$@";;
     *) usage;;
     esac
 }
@@ -16,7 +17,8 @@ Usage: pdf cmd
 
 Commands:
 
-    split images [args...]
+    split images [ARGS...]
+    two-per-sheet INPUT OUTPUT N_PAGES
 EOF
     return 1
 }
@@ -32,6 +34,20 @@ split() {
 
 split_images() {
     exec convert -verbose -density 150 -quality 100 -sharpen 0x1.0 "$@"
+}
+
+two_per_sheet() {
+    local input=$1 n_pages=$2 h p0 p1 pages
+    h=$(((n_pages + 1) / 2))
+    p0=$(seq 1 "$h")
+    p1=$(seq "$((h + 1))" "$n_pages"; ((n_pages % 2)) && echo '{}')
+    pages=$( \
+        paste - <<< $p0 <(tac <<< $p1) \
+            | awk -v OFS=, 'NR%2{print($1,$2);next}{print($2,$1)}' \
+            | paste --serial --delimiter ,)
+    pdfjam --reflect true "$input" "$pages" -o /dev/stdout \
+        | pdfjam --landscape --nup 2x1 -o /dev/stdout \
+        | pdfjam --suffix nup --reflect true --fitpaper true -o /dev/stdout
 }
 
 main "$@"
