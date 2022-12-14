574bce1a404ccdb247e37998cc20f9c210195dcd
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2022-12-14 16:33:09 +0000

    scripts/pdf: replace booklet command

--- scripts/pdf.sh
+++ scripts/pdf.sh
@@ -5,8 +5,8 @@ main() {
     [[ "$#" -eq 0 ]] && usage
     local cmd=$1; shift
     case "$cmd" in
+    booklet) booklet "$@";;
     split) split "$@";;
-    two-per-sheet) two_per_sheet "$@";;
     *) usage;;
     esac
 }
@@ -17,8 +17,8 @@ Usage: pdf cmd
 
 Commands:
 
+    booklet INPUT N_PAGES
     split images [ARGS...]
-    two-per-sheet INPUT OUTPUT N_PAGES
 EOF
     return 1
 }
@@ -36,18 +36,11 @@ split_images() {
     exec convert -verbose -density 150 -quality 100 -sharpen 0x1.0 "$@"
 }
 
-two_per_sheet() {
-    local input=$1 n_pages=$2 h p0 p1 pages
-    h=$(((n_pages + 1) / 2))
-    p0=$(seq 1 "$h")
-    p1=$(seq "$((h + 1))" "$n_pages"; ((n_pages % 2)) && echo '{}')
-    pages=$( \
-        paste - <<< $p0 <(tac <<< $p1) \
-            | awk -v OFS=, 'NR%2{print($1,$2);next}{print($2,$1)}' \
-            | paste --serial --delimiter ,)
-    pdfjam --reflect true "$input" "$pages" -o /dev/stdout \
-        | pdfjam --landscape --nup 2x1 -o /dev/stdout \
-        | pdfjam --suffix nup --reflect true --fitpaper true -o /dev/stdout
+booklet() {
+    local input=$1 n; shift
+    n=$(pdfinfo "$input" | awk -F ':\\s*' '$1 == "Pages" { print $2 }')
+    # https://github.com/rrthomas/pdfjam-extras.git
+    ./pdfbook --signature "$n" "$input" "$@"
 }
 
 main "$@"
