c354ee49a8a5e3d7b5e75b19bc0739863da4552b
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2023-09-22 14:37:58 +0200

    scripts: tsp

---
+++ scripts/tsp.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -euo pipefail
+
+main() {
+    [[ "$#" -eq 0 ]] && usage
+    local cmd=$1; shift
+    case "$cmd" in
+    pop) pop "$@";;
+    *) usage;;
+    esac
+}
+
+usage() {
+    cat >&2 <<EOF
+Usage: $0 CMD ARGS...
+
+Commands:
+
+    pop
+EOF
+    return 1
+}
+
+pop() {
+    local i
+    i=$(tsp -l | awk '$2 == "finished" { print $1; exit }')
+    [[ "$i" ]] || return
+    tsp -c "$i" || true
+    tsp -r "$i"
+}
+
+main "$@"
