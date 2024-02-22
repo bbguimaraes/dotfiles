5166ce066cbe65ea313c3cb1683a4fc3f2d1aeb1
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2024-02-22 22:40:41 +0100

    scripts/date: remove redundant command

--- scripts/date.sh
+++ scripts/date.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 set -euo pipefail
 
-d='date --rfc-3339=seconds'
-while read n tz; do
-    printf '%2s ' "$n"
-    TZ=$tz $d
-done <<'EOF'
+main() {
+    [[ "$#" -eq 0 ]] && { cmd_date; return; }
+    local cmd=$1; shift
+    case "$cmd" in
+    *) usage;;
+    esac
+}
 
+usage() {
+    cat >&2 <<EOF
+Usage: $0 [CMD ARGS...]
+EOF
+    return 1
+}
+
+cmd_date() {
+    local d='date --rfc-3339=seconds' n tz
+    while read n tz; do
+        printf '%2s ' "$n"
+        TZ=$tz $d
+    done <<'EOF'
 P US/Pacific
 E US/Eastern
 BR America/Sao_Paulo
 CE Europe/Prague
 EOF
+}
+
+main "$@"
