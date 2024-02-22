4c564d140d6d1dc710a60422a2e48255346ae8c9
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2024-02-22 22:43:18 +0100

    scripts/date: add diff command

--- scripts/date.sh
+++ scripts/date.sh
@@ -5,6 +5,7 @@ main() {
     [[ "$#" -eq 0 ]] && { cmd_date; return; }
     local cmd=$1; shift
     case "$cmd" in
+    diff) cmd_diff "$@";;
     *) usage;;
     esac
 }
@@ -12,6 +13,10 @@ main() {
 usage() {
     cat >&2 <<EOF
 Usage: $0 [CMD ARGS...]
+
+Commands:
+
+    diff T0 T1
 EOF
     return 1
 }
@@ -27,6 +32,21 @@ E US/Eastern
 BR America/Sao_Paulo
 CE Europe/Prague
 EOF
+}
+
+cmd_diff() {
+    [[ "$#" -eq 2 ]] || usage
+    local t0=$2 t1=$1
+    python -c "$(cat <<'EOF'
+import datetime
+import sys
+
+_, t0, t1 = sys.argv
+d = datetime.datetime
+f = '%Y-%m-%d'
+print(d.strptime(t1, f) - d.strptime(t0, f))
+EOF
+)" "$t0" "$t1"
 }
 
 main "$@"
