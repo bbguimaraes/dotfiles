1cb21d7ef254f308dddaf19a0535e3bf4e2dacbf
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2024-03-11 13:32:00 +0100

    scripts/todo: add command to generate week

--- scripts/todo.sh
+++ scripts/todo.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+DIR=~/n/archivum/todo
+DIES=(lunae martis mercurii iovis veneris saturnis solis)
+
 main() {
     [[ "$#" -eq 0 ]] && { todo; return; }
     local cmd=$1; shift
     case "$cmd" in
     log) log "$@";;
+    week) week "$@";;
     *) usage;;
     esac
 }
@@ -17,6 +21,7 @@ Usage: $0 [CMD ARGS...]
 Commands:
 
     log
+    week DAYS_AHEAD
 EOF
     return 1
 }
@@ -31,6 +36,18 @@ log() {
     local log='journalctl --output short-iso'
     { $log --until today --lines 1 && $log --since today --lines +1; } \
         | sed --expression 's/^.*T//;s/ .*//'
+}
+
+week() {
+    [[ "$#" -gt 1 ]] && usage
+    local i offset today day
+    offset=${1-0}
+    today=$(date +%d --date "+${offset} days")
+    day=$(<"$DIR/dies.txt")
+    for i in {0..6}; do
+        echo "- $((today + i)) ${DIES[i]}"
+        sed 's/^/  /' <<< "$day"
+    done
 }
 
 main "$@"
