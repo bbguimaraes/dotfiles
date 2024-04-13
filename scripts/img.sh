4ebb1c25c05f9e892daa3489d2de1537fafbb370
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2024-04-13 12:23:06 +0200

    scripts/img: add command to rename images

--- scripts/img.sh
+++ scripts/img.sh
@@ -6,6 +6,7 @@ main() {
     local cmd=$1; shift
     case "$cmd" in
     date) cmd_date "$@";;
+    rename) cmd_rename "$@";;
     *) usage
     esac
 }
@@ -17,6 +18,7 @@ Usage: $0 CMD ARG...
 Commands:
 
     date FILE...
+    rename [-v|--verbose|-n|--dry-run] FILE...
 EOF
     return 1
 }
@@ -28,6 +30,30 @@ cmd_date() {
         date=$(identify -format '%[date:create]' "$x")
         name=$(date --utc +"%Y%m%d_%H%M%S")
         echo "$name"
+    done
+}
+
+cmd_rename() {
+    local verbose= dry_run=
+    while [[ "$#" -ne 0 ]]; do
+        case "$1" in
+        -v|--verbose) verbose=1; shift;;
+        -n|--dry-run) dry_run=1; shift;;
+        --) shift; break;;
+        -*) usage;;
+        *) break;;
+        esac
+    done
+    local x ext dst
+    for x; do
+        ext=${x##*.}
+        if [[ "$x" == "$ext" ]]; then
+            echo >&2 "$x: no extension"
+            return 1
+        fi
+        dst=IMG_$(cmd_date "$x").$ext
+        [[ "$verbose" ]] && echo "$x" "$dst"
+        [[ "$dry_run" ]] || mv --interactive "$x" "$dst"
     done
 }
 
