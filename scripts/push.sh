b4a50231a8e5bbd6dec2f743ce0809535fa7eb2f
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2024-04-13 12:09:59 +0200

    scripts/push: extract functions

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,33 +1,47 @@
 #!/bin/bash
 set -euo pipefail
 
-args=()
-while [[ "$#" -gt 0 ]]; do
-    if [[ "$1" == -- ]]; then
+main() {
+    local args=() cred x
+    while [[ "$#" -gt 0 ]]; do
+        if [[ "$1" == -- ]]; then
+            shift
+            break
+        fi
+        args+=("$1")
         shift
-        break
+    done
+    command pass show test > /dev/null
+    if [[ "$#" -eq 0 ]]; then
+        set $(git remote)
     fi
-    args+=("$1")
-    shift
-done
-command pass show test > /dev/null
-if [[ "$#" -eq 0 ]]; then
-    set $(git remote)
-fi
-for x; do
-    case "$PWD" in
-    */src/es/*) n=comp/es/gitlab;;
-    *)
-        case "$(git remote get-url "$x")" in
-        *github.com/*) n=comp/github/oauth_token;;
-        *gitlab.bbguimaraes.com/*) n=bbguimaraes.com/gitlab/bbguimaraes;;
-        *git.bbguimaraes.com/*) n=bbguimaraes.com/git;;
-        esac
+    for x; do
+        cred=$(get_cred "$PWD" "$x")
+        push "$x" "$cred" "${args[@]}"
+    done
+}
+
+get_cred() {
+    local dir=$1 repo=$2
+    case "$dir" in
+    */src/es/*) echo comp/es/gitlab; return;;
     esac
-    [[ "$n" ]]
+    case "$(git remote get-url "$x")" in
+    *github.com/*) echo comp/github/oauth_token; return;;
+    *gitlab.bbguimaraes.com/*) echo bbguimaraes.com/gitlab/bbguimaraes; return;;
+    *git.bbguimaraes.com/*) echo bbguimaraes.com/git; return;;
+    esac
+    echo >&2 "unknown repository: $repo"
+    return 1
+}
+
+push() {
+    local target=$1 cred=$2; shift 2
     git \
         -c "credential.helper=$(printf \
             '!f() { printf password= && pass show %s; } && f' \
-            "$n")" \
-        push "$x" "${args[@]}"
-done
+            "$cred")" \
+        push "$target" "$@"
+}
+
+main "$@"
