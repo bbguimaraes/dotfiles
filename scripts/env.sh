ccae2974ebc02af0b932c6a18202c232b36fd810
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2024-04-23 17:11:14 +0200

    custos: use configuration file

--- scripts/env.sh
+++ scripts/env.sh
@@ -14,7 +14,7 @@ main() {
     tmux respawn-pane -t "$target:0" -k d todo
     tmux split-window -t "$target:0.0" -l 1000 top -o %CPU
     tmux split-window -t "$target:0.1" -l 1000 journalctl -f
-    tmux split-window -t "$target:0.2" -l 1000 d custos
+    tmux split-window -t "$target:0.2" -l 1000 custos --clear
     tmux select-layout -t "$target:0" main-vertical
     tmux new-window -t "$target:1" weechat
     tmux split-window -t "$target:1.0" -l 1000 d mutt
