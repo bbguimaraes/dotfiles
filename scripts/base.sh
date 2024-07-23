2697b23e92a629a3e0f1c3e457d8a87d91bb6e27
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2024-07-23 19:15:37 +0200

    scripts: add base script template

---
+++ scripts/base.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+main() {
+    [[ "$#" -eq 0 ]] && usage
+    local cmd=$1; shift
+    case "$cmd" in
+    test) cmd_test;;
+    *) usage;;
+    esac
+}
+
+usage() {
+    cat >&2 <<EOF
+Usage: $0 CMD ARG...
+
+Commands:
+EOF
+    return 1
+}
+
+cmd_test() {
+    [[ "$#" -eq 0 ]] && usage
+    local cmd=$1; shift
+    case "$cmd" in
+    *) usage;;
+    esac
+}
+
+main "$@"
