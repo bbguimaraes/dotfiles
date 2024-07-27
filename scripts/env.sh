6e34d2ba3acab0781f54e686457c8a0f55f08533
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2024-07-27 14:28:07 +0200

    scripts/env: extract functions

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,32 +1,66 @@
 #!/bin/bash
 set -euo pipefail
 
-SESSION=0
+SESSION_MAIN=0
 
 main() {
-    local SESSION=0 target
-    if tmux has-session -t "$SESSION" &> /dev/null; then
-        attach
-    fi
-    local target=$SESSION
-    tmux new-session -s "$SESSION" -d
-    tmux rename-window -t "$target" ''
-    tmux respawn-pane -t "$target:0" -k d todo
-    tmux split-window -t "$target:0.0" -l 1000 top -o %CPU
-    tmux split-window -t "$target:0.1" -l 1000 journalctl -f
-    tmux split-window -t "$target:0.2" -l 1000 custos --clear
-    tmux select-layout -t "$target:0" main-vertical
-    tmux new-window -t "$target:1" weechat
-    tmux rename-window -t "$target:1" ''
-    tmux split-window -t "$target:1.0" -l 1000 d mutt
-    tmux split-window -t "$target:1.1" -l 1000 newsboat
-    tmux split-window -t "$target:1.2" -l 1000 ikhal
-    tmux select-layout -t "$target:1" main-horizontal
-    tmux new-window -t "$target:2" subs tui
-    tmux rename-window -t "$target:2" 'v'
-    attach
+    session_main
+    exec tmux attach -t "$SESSION_MAIN"
 }
 
-attach() { exec tmux attach -t "$SESSION"; }
+session_main() {
+    local target=$SESSION_MAIN
+    has_session "$target" && return
+    new_session "$target"
+    rename_window "$target" ''
+    respawn_pane "$target:0" d todo
+    split_window "$target:0.0" top -o %CPU
+    split_window "$target:0.1" journalctl -f
+    split_window "$target:0.2" custos --clear
+    select_layout "$target:0" main-vertical
+    new_window "$target:1" weechat
+    rename_window "$target:1" ''
+    split_window "$target:1.0" d mutt
+    split_window "$target:1.1" newsboat
+    split_window "$target:1.2" ikhal
+    select_layout "$target:1" main-horizontal
+    new_window "$target:2" subs tui
+    rename_window "$target:2" v
+}
+
+has_session() {
+    tmux has-session -t "$1" &> /dev/null
+}
+
+new_session() {
+    tmux new-session -s "$1" -d
+}
+
+rename_window() {
+    tmux rename-window -t "$1" "$2"
+}
+
+respawn_pane() {
+    local target=$1; shift
+    tmux respawn-pane -t "$target" -k $@
+}
+
+split_window() {
+    local target=$1; shift
+    tmux split-window -t "$target" -l 1000 $@
+}
+
+new_window() {
+    local target=$1; shift
+    tmux new-window -t "$target" "$@"
+}
+
+rename_window() {
+    tmux rename-window -t "$1" "$2"
+}
+
+select_layout() {
+    tmux select-layout -t "$1" "$2"
+}
 
 main "$@"
