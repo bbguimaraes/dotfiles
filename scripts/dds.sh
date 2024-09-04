5015f309c52dcd9ea6485dfdec13c3a5e924e7fa
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2024-09-04 11:50:47 +0200

    dds: add SSH command

--- scripts/dds.sh
+++ scripts/dds.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+INFRA=$HOME/dds/infra
+declare -A INVENTORY=(
+    [dev]=eu-central-dev
+    [prod]=eu-central
+)
+
 main() {
     [[ "$#" -eq 0 ]] && usage
     local cmd=$1; shift
     case "$cmd" in
+    ssh) cmd_ssh "$@";;
     weechat) exec weechat --dir ~/dds/weechat "$@";;
     *) usage;;
     esac
@@ -16,9 +23,21 @@ Usage: $0 CMD ARG...
 
 Commands:
 
+    ssh dev|prod ARG...
     weechat [ARG...]
 EOF
     return 1
+}
+
+cmd_ssh() {
+    [[ "$#" -eq 0 ]] && usage
+    local cmd=$1; shift
+    case "$cmd" in
+    dev|prod) exec ssh \
+        -F "$INFRA/inventory/${INVENTORY[$cmd]}/ssh_config" \
+        "$@";;
+    *) usage;;
+    esac
 }
 
 main "$@"
