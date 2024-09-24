ad191e6edc384f10a25fba98d369768102d11ff0
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2024-09-24 18:10:40 +0200

    scripts/tsp: add command to tail all logs

--- scripts/tsp.sh
+++ scripts/tsp.sh
@@ -5,6 +5,7 @@ main() {
     [[ "$#" -eq 0 ]] && usage
     local cmd=$1; shift
     case "$cmd" in
+    log) log "$@";;
     pop) pop "$@";;
     *) usage;;
     esac
@@ -16,9 +17,19 @@ Usage: $0 CMD ARG...
 
 Commands:
 
+    log
     pop
 EOF
     return 1
+}
+
+log() {
+    [[ "$#" -eq 0 ]] || usage
+    local x
+    for x in $(unfinished_ids); do
+        tmux split-window -l 1000 tsp -c "$x"
+    done
+    exec tmux select-layout even-vertical
 }
 
 pop() {
@@ -32,6 +43,10 @@ pop() {
 
 finished_ids() {
     tsp -l | awk '$2 == "finished" { print $1 }'
+}
+
+unfinished_ids() {
+    tsp -l | awk '$2 == "running" { print $1 }'
 }
 
 main "$@"
