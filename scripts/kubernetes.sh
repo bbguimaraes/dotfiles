5308d555cf91119dc3193c9e241854d139c659b1
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2024-12-24 11:37:59 +0100

    scripts/kubernetes: add commands for volumes

--- scripts/kubernetes.sh
+++ scripts/kubernetes.sh
@@ -6,6 +6,7 @@ main() {
     local cmd=$1; shift
     case "$cmd" in
     cm|secret) cm_secret "$@";;
+    pvc) pvc "$@";;
     *) usage;;
     esac
 }
@@ -18,6 +19,7 @@ Commands:
 
     cm|secret decode|encode|keys|size|sizes
     cm|secret key KEY
+    pvc clear|copy|ls NS SRC DST
 EOF
     return 1
 }
@@ -55,6 +57,121 @@ if .data then .data |= map_values(@base64d) else . end
             | sort --numeric-sort;;
     *) usage;;
     esac
+}
+
+pvc() {
+    [[ "$#" -eq 0 ]] && usage
+    local cmd=$1; shift
+    case "$cmd" in
+    clear) pvc_clear "$@";;
+    copy) pvc_copy "$@";;
+    ls) pvc_ls "$@";;
+    *) usage;;
+    esac
+}
+
+pvc_job() {
+    local ns=$1 name=$2 pod=$3
+    kubectl --namespace "$ns" create --filename - <<< $pod
+    kubectl --namespace "$ns" logs --follow "job/$name"
+}
+
+pvc_clear() {
+    [[ "$#" -eq 2 ]] || usage
+    local ns=$1 vol=$2 job=pvc-clear
+    pvc_job "$ns" "$job" "$(cat <<EOF
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: $job
+spec:
+  template:
+    spec:
+      restartPolicy: Never
+      volumes:
+      - name: vol
+        persistentVolumeClaim:
+          claimName: $vol
+      containers:
+      - name: $job
+        image: archlinux
+        command:
+        - bash
+        args:
+        - -c
+        - rm -rfv /mnt/vol/* /mnt/vol/.[^.]*
+        volumeMounts:
+        - mountPath: /mnt/vol
+          name: vol
+EOF
+)"
+}
+
+pvc_copy() {
+    [[ "$#" -eq 3 ]] || usage
+    local ns=$1 src=$2 dst=$3 job=pvc-copy
+    pvc_job "$ns" "$job" "$(cat <<EOF
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: $job
+spec:
+  template:
+    spec:
+      restartPolicy: Never
+      volumes:
+      - name: src
+        persistentVolumeClaim:
+          claimName: $src
+      - name: dst
+        persistentVolumeClaim:
+          claimName: $dst
+      containers:
+      - name: $job
+        image: archlinux
+        command:
+        - bash
+        args:
+        - -c
+        - tar -C /mnt/src -c . | tar -C /mnt/dst -vx
+        volumeMounts:
+        - mountPath: /mnt/src
+          name: src
+        - mountPath: /mnt/dst
+          name: dst
+EOF
+)"
+}
+
+pvc_ls() {
+    [[ "$#" -eq 2 ]] || usage
+    local ns=$1 vol=$2 job=pvc-ls
+    pvc_job "$ns" "$job" "$(cat <<EOF
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: $job
+spec:
+  template:
+    spec:
+      restartPolicy: Never
+      volumes:
+      - name: vol
+        persistentVolumeClaim:
+          claimName: $vol
+      containers:
+      - name: $job
+        image: archlinux
+        command:
+        - bash
+        args:
+        - -c
+        - find /mnt/vol | sort
+        volumeMounts:
+        - mountPath: /mnt/vol
+          name: vol
+EOF
+)"
 }
 
 main "$@"
